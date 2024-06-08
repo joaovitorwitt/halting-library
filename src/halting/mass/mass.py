ec2bfa1f21adcee252e4827613da63495b94e89d
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-06-08 14:38:06 -0300

    docstring

--- src/halting/mass/mass.py
+++ src/halting/mass/mass.py
@@ -5,13 +5,37 @@ class MassConversion(object):
 
     def convert_mg_to_g(self, mg: int | float) -> int | float:
         """
-        divide the value by 1000 (or multiply by 0.001)
+        Converts milligrams to grams.
+
+        This function converts milligrams to grams by dividing the value by 1000.
+
+        Args:
+            mg (int | float): The value of milligrams to be converted.
+        
+        Returns:
+            int | float: The converted value as grams
+
+        Usage:
+            >>> from halting.mass.mass import MassConversion
+            >>> mass_conversion = MassConversion()
+            >>> mass_conversion.convert_mg_to_g(1000)
+            1
         """
         return mg / 1000
-     
+    
     def convert_g_to_kg(self, g: int | float) -> int | float:
         """
-        divide the value by 1000
+        Converts grams to kilograms
+
+        This function takes a value of grams and converted it to kilograms
+        the standard SI unit, by dividing the value by 1000.
+
+        Args:
+
+        Returns:
+
+        Usage:
+            >>> from halting.mass.mass import MassConversion
         """
         return g / 1000
     
