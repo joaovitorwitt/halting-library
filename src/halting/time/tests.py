ed201811eabfff8e8afe156a72a5380ada6205f4
joaowitt <jwitt@everysk.com>
2024-06-17 10:37:31 -0300

    time conversion template

---
+++ src/halting/time/tests.py
@@ -0,0 +1,166 @@
+###############################################################################
+# Imports
+###############################################################################
+import unittest
+from unittest import TestCase
+from src.halting.time.time import TimeConversion
+
+###############################################################################
+# Time Conversion Test Case Implementation
+###############################################################################
+class TimeConversionTestCase(TestCase):
+    
+    def setUp(self) -> None:
+        self.time_conversion = TimeConversion()
+        return super().setUp()
+
+    ###############################################################################
+    # Milliseconds to Seconds Test Case
+    ###############################################################################
+    def test_convert_milliseconds_to_seconds(self):
+        pass
+
+    def test_convert_milliseconds_to_seconds_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_milliseconds_to_seconds_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Seconds to Minutes Test Case
+    ###############################################################################
+    def test_convert_seconds_to_minutes(self):
+        pass
+
+    def test_convert_seconds_to_minutes_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_seconds_to_minutes_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Minutes to Hours Test Case
+    ###############################################################################
+    def test_convert_minutes_to_hours(self):
+        pass
+
+    def test_convert_minutes_to_hours_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_minutes_to_hours_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Hours to Minutes Test Case
+    ###############################################################################
+    def test_convert_hours_to_minutes(self):
+        pass
+
+    def test_convert_hours_to_minutes_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_hours_to_minutes_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Minutes to Seconds Test Case
+    ###############################################################################
+    def test_convert_minutes_to_seconds(self):
+        pass
+
+    def test_convert_minutes_to_seconds_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_minutes_to_seconds_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Seconds to Milliseconds Test Case
+    ###############################################################################
+    def test_convert_seconds_to_milliseconds(self):
+        pass
+
+    def test_convert_seconds_to_milliseconds_with_invalid_type_raises_error(self):
+        pass
+    
+    def test_convert_seconds_to_milliseconds_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Milliseconds to Minutes Test Case
+    ###############################################################################
+    def test_convert_milliseconds_to_minutes(self):
+        pass
+
+    def test_convert_seconds_to_milliseconds_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_seconds_to_milliseconds_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Milliseconds to Hours Test Case
+    ###############################################################################
+    def test_convert_milliseconds_to_hours(self):
+        pass
+
+    def test_convert_milliseconds_to_hours_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_milliseconds_to_hours_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Hours to Seconds Test Case
+    ###############################################################################
+    def test_convert_hours_to_seconds(self):
+        pass
+
+    def test_convert_hours_to_seconds_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_hours_to_seconds_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Hours to Milliseconds Test Case
+    ###############################################################################
+    def test_convert_hours_to_milliseconds(self):
+        pass
+
+    def test_convert_hours_to_milliseconds_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_hours_to_milliseconds_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Seconds to Hours Test Case
+    ###############################################################################
+    def test_convert_seconds_to_hours(self):
+        pass
+
+    def test_convert_seconds_to_hours_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_seconds_to_hours_with_negative_value_raises_error(self):
+        pass
+
+    ###############################################################################
+    # Minutes to Milliseconds Test Case
+    ###############################################################################
+    def test_convert_minutes_to_milliseconds(self):
+        pass
+
+    def test_convert_minutes_to_milliseconds_with_invalid_type_raises_error(self):
+        pass
+
+    def test_convert_minutes_to_milliseconds_with_negative_value_raises_error(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+    
+
+
+
---
+++ src/halting/time/time.py
@@ -0,0 +1,51 @@
+###############################################################################
+# Imports
+###############################################################################
+
+
+###############################################################################
+# Time Conversion Class Implementation
+###############################################################################
+
+#
+#
+# ms - s - min - hour
+#
+#
+class TimeConversion(object):
+
+    def convert_milliseconds_to_seconds(self, milliseconds: int | float) -> int | float:
+        pass
+
+    def convert_seconds_to_minutes(self, seconds: int | float) -> int | float:
+        pass
+
+    def convert_minutes_to_hours(self, minutes: int | float) -> int | float:
+        pass
+
+    def convert_hours_to_minutes(self, hours: int | float) -> int | float:
+        pass
+
+    def convert_minutes_to_seconds(self, minutes: int | float) -> int | float:
+        pass
+
+    def convert_seconds_to_milliseconds(self, seconds: int | float) -> int | float:
+        pass
+
+    def convert_milliseconds_to_minutes(self, milliseconds: int | float) -> int | float:
+        pass
+
+    def convert_milliseconds_to_hours(self, milliseconds: int | float) -> int | float:
+        pass
+
+    def convert_hours_to_seconds(self, hours: int | float) -> int | float:
+        pass
+
+    def convert_hours_to_milliseconds(self, hours: int | float) -> int | float:
+        pass
+
+    def convert_seconds_to_hours(self, seconds: int | float) -> int | float:
+        pass
+
+    def convert_minutes_to_milliseconds(self, minutes: int | float) -> int | float:
+        pass
