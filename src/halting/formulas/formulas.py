993af89f21753cda911f9d6646a456b78ea787f3
joaowitt <jwitt@everysk.com>
2024-06-20 13:49:00 -0300

    Adding more volume formulas

--- src/halting/formulas/formulas.py
+++ src/halting/formulas/formulas.py
@@ -63,29 +63,87 @@ class GeneralFormulas(BaseHalting):
 
     def calculate_volume_of_a_cone(self, base_radius: int | float, height: int | float) -> int | float:
         """
-        _summary_
+        Calculate the volume of a cone
 
         Args:
-            base_radius (int | float): _description_
-            height (int | float): _description_
+            base_radius (int | float): The radius of the base of the cone.
+            height (int | float): The height of the cone
 
         Returns:
-            int | float: _description_
+            int | float: The calculated volume of the cone.
 
         Example:
-            >>>
+            >>> from src.halting.formulas.formulas import GeneralFormulas()
+            >>> general_formulas = GeneralFormulas()
+            >>> general_formulas.calculate_volume_of_a_cone(2, 2)
+            8.38
         """
+        cone_formula = (pi * pow(base_radius, 2) * height) / 3
+        return cone_formula
 
     def calculate_volume_of_a_rectangle(self, length: int | float, height: int | float, width: int | float) -> int | float:
-        pass
+        """
+        Calculate the volume of a rectangle
+
+        Args:
+            length (int | float): The length of the rectangle
+            height (int | float): The height of the rectangle
+            width (int | float): The width of the rectangle
+
+        Returns:
+            int | float: The final calculated volume for the rectangle.
+
+        Example:
+            >>> from src.halting.formulas.formulas import GeneralFormulas()
+            >>> general_formulas = GeneralFormulas()
+            >>> general_formulas.calculate_volume_of_a_rectangle(2, 2, 2)
+            8
+        """
+        rectangle_formula = length * height * width
+        return rectangle_formula
 
     def calculate_spherical_cap_volume(self, base_radius: int | float, height: int | float, ball_radius: int | float) -> int | float:
-        pass
+        """
+        Function calculates the volume of a spherical cap
+
+        Args:
+            base_radius (int | float): Base radius of the cap
+            ball_radius (int | float): Radius of the ball
+            height (int | float): Height from the base until the the top.
+
+        Returns:
+            int | float: The calculated volume for the spherical cap.
+
+        Example:
+            >>> from src.halting.formulas.formulas import GeneralFormulas()
+            >>> general_formulas = GeneralFormulas()
+            >>> general_formulas.calculate_spherical_cap_volume(2, 2, 2)
+            16.75516
+        """
+        spherical_cap = (pi / 3) * pow(height, 2) * (3*ball_radius - height)
+        return spherical_cap
 
     def calculate_volume_of_a_capsule(self, base_radius: int | float, height: int | float):
+        """
+        Calculates the volume of a capsule.
+
+        Args:
+            base_radius (int | float): The radius of the capsule.
+            height (int | float): The total height of the capsule.
+
+        Returns:    
+            >>> from src.halting.formulas.formulas import GeneralFormulas
+            >>> general_formulas = GeneralFormulas()
+            >>> general_formulas.calculate_volume_of_a_capsule(2, 2)
+            58.64
+        """
+
+    def calculate_conical_frustum_volume(self, top_radius: int | float, bottom_radius: int | float, height: int | float) -> int | float:
         pass
+
 
     
 
 # AREA CALCULATOR - https://www.calculator.net/area-calculator.html
 # VOLUEM CALCULATOR - https://www.calculator.net/volume-calculator.html
+# ALGEBRA SECTION - https://www.calculatorsoup.com/calculators/algebra/
