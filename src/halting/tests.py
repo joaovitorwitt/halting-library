c1f54fadb11eec21c89bc42cddd63afce80491aa
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-06-21 00:53:13 -0300

    up

--- src/halting/tests.py
+++ src/halting/tests.py
@@ -4,6 +4,7 @@
 from src.halting.length.tests import *
 from src.halting.mass.tests import *
 from src.halting.time.tests import *
+from src.halting.formulas.tests import *
 
 """
 missing imports from other modules as well
