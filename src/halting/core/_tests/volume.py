7c3614416937214bbdb32028bf0f3bc48ca7eaeb
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-08-08 22:19:47 -0300

    Refactor: updating test cases for volume method

--- src/halting/core/_tests/volume.py
+++ src/halting/core/_tests/volume.py
@@ -15,7 +15,7 @@ class VolumeTestCase(TestCase):
         self.volume = Volume()
         return super().setUp()
     
-
+ 
     def test_get_cylinder_volume(self):
         self.assertEqual(self.volume.get_cylinder_volume(2, 2), 25.13)
         self.assertEqual(self.volume.get_cylinder_volume(598, 21), 828494.53)
@@ -46,71 +46,32 @@ class VolumeTestCase(TestCase):
         self.assertEqual(self.volume.get_cone_volume(12.4, 27.32, 10), 4398.9870482947)
 
 
-    def test_calculate_rectangle_volume(self):
-        self.assertEqual(self.volume.calculate_rectangle_volume(2, 3, 4), 24)
-        self.assertEqual(self.volume.calculate_rectangle_volume(1.92, 9.38, 2.5), 45.024)
-        self.assertEqual(self.volume.calculate_rectangle_volume(134, 928, 292), 36310784)
+    def test_get_rectangle_volume(self):
+        self.assertEqual(self.volume.get_rectangle_volume(2, 3, 4), 24)
+        self.assertEqual(self.volume.get_rectangle_volume(1.92, 9.38, 2.5), 45.02)
+        self.assertEqual(self.volume.get_rectangle_volume(134, 928, 292), 36310784)
+        self.assertEqual(self.volume.get_rectangle_volume(2.2,0.98,22.3, 4), 48.0788)
 
 
-    ##################################################
-    #  Spherical Cap Volume Test Case Implementation
-    ##################################################
-    def test_calculate_spherical_cap_volume(self):
-        self.assertEqual(self.volume.calculate_spherical_cap_volume(2, 2), 16.7552)
-        self.assertEqual(self.volume.calculate_spherical_cap_volume(32.8, 23.9), 43825.4018)
-        self.assertEqual(self.volume.calculate_spherical_cap_volume(3.65, 2.9), 70.454)
+    def test_get_spherical_cap_volume(self):
+        self.assertEqual(self.volume.get_spherical_cap_volume(2, 2, 2), 16.76)
+        self.assertEqual(self.volume.get_spherical_cap_volume(2, 2, 4), 16.7552)
+        self.assertEqual(self.volume.get_spherical_cap_volume(32.8, 23.9), 43825.4)
+        self.assertEqual(self.volume.get_spherical_cap_volume(3.65, 2.9), 70.45)
 
-    def test_calculate_spherical_cap_volume_with_invalid_type_raises_error(self):
-        with self.assertRaises(TypeError) as context:
-            self.volume.calculate_spherical_cap_volume(2, (2, 2))
 
-        self.assertEqual(str(context.exception), "'tuple' is not allowed, only integer or float")
+    def test_get_capsule_volume(self):
+        self.assertEqual(self.volume.get_capsule_volume(2, 2), 58.64)
+        self.assertEqual(self.volume.get_capsule_volume(0.98, 2.9), 12.69)
+        self.assertEqual(self.volume.get_capsule_volume(0.98, 2.9, 4), 12.6923)
+        self.assertEqual(self.volume.get_capsule_volume(83.202, 98, 6), 4543920.041935)
 
-    def test_calculate_spherical_cap_volume_with_negative_value_raises_error(self):
-        with self.assertRaises(ValueError) as context:
-            self.volume.calculate_spherical_cap_volume(2, -2)
 
-        self.assertEqual(str(context.exception), "Negative values are not allowed.")
-
-    ##################################################
-    #  Capsule Volume Test Case Implementation
-    ##################################################
-    def test_calculate_capsule_volume(self):
-        self.assertEqual(self.volume.calculate_capsule_volume(2, 2), 58.6431)
-        self.assertEqual(self.volume.calculate_capsule_volume(0.98, 2.9), 12.6923)
-        self.assertEqual(self.volume.calculate_capsule_volume(83.202, 98), 4543920.0419)
-
-    def test_calculate_capsule_volume_with_invalid_type_raises_error(self):
-        with self.assertRaises(TypeError) as context:
-            self.volume.calculate_capsule_volume(2, 'carnival')
-        
-        self.assertEqual(str(context.exception), "'str' is not allowed, only integer or float")
-
-    def test_calculate_capsule_volume_with_negative_value_raises_error(self):
-        with self.assertRaises(ValueError) as context:
-            self.volume.calculate_capsule_volume(2, -1)
-
-        self.assertEqual(str(context.exception), "Negative values are not allowed.")
-
-    ##################################################
-    #  Ellipsoid Volume Test Case Implementation
-    ##################################################
-    def test_calculate_ellipsoid_volume(self):
-        self.assertEqual(self.volume.calculate_ellipsoid_volume(2, 22, 12), 2211.6812)
-        self.assertEqual(self.volume.calculate_ellipsoid_volume(1.32, 3, 0.2), 3.3175)
-        self.assertEqual(self.volume.calculate_ellipsoid_volume(2.32, 1.02, 0.22), 2.1807)
-
-    def test_calculate_ellipsoid_volume_with_invalid_type_raises_error(self):
-        with self.assertRaises(TypeError) as context:
-            self.volume.calculate_ellipsoid_volume(2, 2, 'evea')
-
-        self.assertEqual(str(context.exception), "'str' is not allowed, only integer or float")
-
-    def test_calculate_ellipsoid_volume_with_negative_value_raises_error(self):
-        with self.assertRaises(ValueError) as context:
-            self.volume.calculate_ellipsoid_volume(2, 2, -1)
-
-        self.assertEqual(str(context.exception), "Negative values are not allowed.")
+    def test_get_ellipsoid_volume(self):
+        self.assertEqual(self.volume.get_ellipsoid_volume(2, 22, 12), 2211.68)
+        self.assertEqual(self.volume.get_ellipsoid_volume(2, 22, 12, 4), 2211.6812)
+        self.assertEqual(self.volume.get_ellipsoid_volume(1.32, 3, 0.2), 3.32)
+        self.assertEqual(self.volume.get_ellipsoid_volume(2.32, 1.02, 0.22), 2.18)
 
 
 if __name__ == "__main__":
--- src/halting/core/volume.py
+++ src/halting/core/volume.py
@@ -46,13 +46,13 @@ class Volume(BaseHalting):
 
         Args:
             radius(int, float): The radius of the sphere
-            rounding_factor (int, optional): The number of decimal places in the final result. Default is 2
+            rounding_factor (int, optional): The number of decimal places in the final result. Default is 2.
             
         Returns:
             int | float: The calculated volume of a sphere.
 
         Example:
-            >>> calculate_volume_of_a_cylinder(3)
+            >>> get_sphere_volume(3)
             113.04
         """
         self.validate_instance((int, float), radius)
@@ -73,7 +73,7 @@ class Volume(BaseHalting):
             int | float: The final calculated volume of the cube.
         
         Example:
-            >>> calculate_volume_of_a_cube(3)
+            >>> get_cube_volume(3)
             27
         """
         self.validate(edge_length)
@@ -119,9 +119,7 @@ class Volume(BaseHalting):
             int | float: The final calculated volume for the rectangle.
 
         Example:
-            >>> from src.halting.formulas.formulas import GeneralFormulas()
-            >>> general_formulas = GeneralFormulas()
-            >>> general_formulas.calculate_volume_of_a_rectangle(2, 2, 2)
+            >>> get_rectangle_volume(2, 2, 2)
             8
         """
         self.validate_instance((int, float), length)
@@ -132,7 +130,7 @@ class Volume(BaseHalting):
         return round(rectangle_formula, rounding_factor)
 
 
-    def calculate_spherical_cap_volume(self, height: int | float, ball_radius: int | float) -> int | float:
+    def get_spherical_cap_volume(self, height: int | float, ball_radius: int | float, rounding_factor: int = 2) -> int | float:
         """
         Function calculates the volume of a spherical cap
 
@@ -140,44 +138,44 @@ class Volume(BaseHalting):
             base_radius (int | float): Base radius of the cap
             ball_radius (int | float): Radius of the ball
             height (int | float): Height from the base until the the top.
+            rounding_factor (int, optional): The number of decimal places the final result should have. Default is 2
 
         Returns:
             int | float: The calculated volume for the spherical cap.
 
         Example:
-            >>> from src.halting.formulas.formulas import GeneralFormulas()
-            >>> general_formulas = GeneralFormulas()
-            >>> general_formulas.calculate_spherical_cap_volume(2, 2, 2)
+            >>> get_spherical_cap_volume(2, 2, 2)
             16.75516
         """
         self.validate(height, ball_radius)
 
         spherical_cap = (settings.PI / 3) * pow(height, 2) * (3*ball_radius - height)
-        return round(spherical_cap, 4)
+        return round(spherical_cap, rounding_factor)
 
-    def calculate_capsule_volume(self, base_radius: int | float, height: int | float) -> int | float:
+
+    def get_capsule_volume(self, base_radius: int | float, height: int | float, rounding_factor: int = 2) -> int | float:
         """
         Calculates the volume of a capsule.
 
         Args:
             base_radius (int | float): The radius of the capsule.
             height (int | float): The total height of the capsule.
+            rounding_factor (int, optional): The number of decimal places the calculation should have. Default is 2
 
         Returns:
             int | float: The calculated capsule volume.
             
         Example:    
-            >>> from src.halting.formulas.formulas import GeneralFormulas
-            >>> general_formulas = GeneralFormulas()
-            >>> general_formulas.calculate_volume_of_a_capsule(2, 2)
+            >>> get_capsule_volume(2, 2)
             58.64
         """
         self.validate(base_radius, height)
 
         capsule = settings.PI * pow(base_radius, 2) * (((4/3)* base_radius) + height)
-        return round(capsule, 4)
+        return round(capsule, rounding_factor)
 
-    def calculate_ellipsoid_volume(self, axis_a: int | float, axis_b: int | float, axis_c: int | float) -> int | float:
+
+    def get_ellipsoid_volume(self, axis_a: int | float, axis_b: int | float, axis_c: int | float, rounding_factor: int = 2) -> int | float:
         """
         Calculate the volume of an ellipsoid
 
@@ -185,17 +183,16 @@ class Volume(BaseHalting):
             axis_a (int | float): The A axis of the ellipsoid.
             axis_b (int | float): The B axis of the ellipsoid.
             axis_c (int | float): The C axis of the ellipsoid.
+            rounding_factor (int, optional): The number of decimal places the result should have. Default is 2.
 
         Returns:
             int | float: The calculated volume of the ellipsoid.
 
         Example:
-            >>> from src.halting.formulas.volume import VolumeFormulas
-            >>> volume = VolumeFormulas()
-            >>> volume.calculate_ellipsoid_volume(2, 2, 2)
+            >>> get_ellipsoid_volume(2, 2, 2)
             33.5103            
         """
         self.validate(axis_a, axis_b, axis_c)
 
         ellipsoid = (4 * settings.PI * axis_a * axis_b * axis_c) / 3
-        return round(ellipsoid, 4)
+        return round(ellipsoid, rounding_factor)
